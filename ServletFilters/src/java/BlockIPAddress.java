a67dbd47f6e9091029b43cceda941e773ba27bb2
Kalpesh Chauhan <63454815+kalpesh915@users.noreply.github.com>
2026-02-10 17:41:55 +0530

    Filters in Servlet

---
+++ ServletFilters/src/java/BlockIPAddress.java
@@ -0,0 +1,35 @@
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class BlockIPAddress implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+        String clientIP = sr.getRemoteAddr();
+        PrintWriter write = sr1.getWriter();
+        
+        if(clientIP.equals("127.0.0.1")){
+            write.println("<h2>Your IP address is blocked by this website</h2>");
+        }else{
+            fc.doFilter(sr, sr1); // Forward request to next filter or servlet
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
---
+++ ServletFilters/src/java/Greetings.java
@@ -0,0 +1,33 @@
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Greetings implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+        String clientIP = sr.getRemoteAddr();
+        PrintWriter write = sr1.getWriter();
+        
+        
+        write.println("<h2>Greetings Called</h2>");
+        
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
